f79812c366e0037aa9f4c8daaff4680438408786
Krystian Żybura <Krystian12324444@gmail.com>
2023-12-20 12:20:08 +0100

    id renamed

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -4,11 +4,11 @@ import newYorkCityImage from "../../assets/newYorkCityImage.jpg";
 import { Image, ImageWrapper } from "./styled";
 
 const Carousel = () => (
-  <div id="carouselExampleIndicators" className="carousel slide">
+  <div id="carouselIndicators" className="carousel slide">
     <div className="carousel-indicators">
       <button
         type="button"
-        data-bs-target="#carouselExampleIndicators"
+        data-bs-target="#carouselIndicators"
         data-bs-slide-to="0"
         className="active"
         aria-current="true"
@@ -16,13 +16,13 @@ const Carousel = () => (
       ></button>
       <button
         type="button"
-        data-bs-target="#carouselExampleIndicators"
+        data-bs-target="#carouselIndicators"
         data-bs-slide-to="1"
         aria-label="Slide 2"
       ></button>
       <button
         type="button"
-        data-bs-target="#carouselExampleIndicators"
+        data-bs-target="#carouselIndicators"
         data-bs-slide-to="2"
         aria-label="Slide 3"
       ></button>
@@ -41,7 +41,7 @@ const Carousel = () => (
     <button
       className="carousel-control-prev"
       type="button"
-      data-bs-target="#carouselExampleIndicators"
+      data-bs-target="#carouselIndicators"
       data-bs-slide="prev"
     >
       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -50,7 +50,7 @@ const Carousel = () => (
     <button
       className="carousel-control-next"
       type="button"
-      data-bs-target="#carouselExampleIndicators"
+      data-bs-target="#carouselIndicators"
       data-bs-slide="next"
     >
       <span className="carousel-control-next-icon" aria-hidden="true"></span>
