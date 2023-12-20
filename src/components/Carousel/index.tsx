81d0865c33d337cf8d85e059ea32aa50f1b25dac
Krystian Żybura <Krystian12324444@gmail.com>
2023-12-20 13:55:33 +0100

    captions added for carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -30,12 +30,48 @@ const Carousel = () => (
     <div className="carousel-inner">
       <ImageWrapper className="carousel-item active">
         <Image src={nothernLightsImage} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block top-0 mt-5">
+          <p className="fs-3 text-uppercase">Discover the hidden world</p>
+          <h1 className="display-1 fw-bolder text-capitalize">
+            The Aurora Tours
+          </h1>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg px-4 py-2 mt-5"
+          >
+            Book a tour
+          </button>
+        </div>
       </ImageWrapper>
       <ImageWrapper className="carousel-item">
         <Image src={treesImage} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block top-0 mt-5">
+          <p className="fs-3 text-uppercase">The season has arrived</p>
+          <h1 className="display-1 fw-bolder text-capitalize">
+            The Aurora Tours
+          </h1>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg px-4 py-2 mt-5"
+          >
+            Book a tour
+          </button>
+        </div>
       </ImageWrapper>
       <ImageWrapper className="carousel-item">
         <Image src={newYorkCityImage} className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block top-0 mt-5">
+          <p className="fs-3 text-uppercase">Discover the hidden world</p>
+          <h1 className="display-1 fw-bolder text-capitalize">
+            The Aurora Tours
+          </h1>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg px-4 py-2 mt-5"
+          >
+            Book a tour
+          </button>
+        </div>
       </ImageWrapper>
     </div>
     <button
