590903938f96ff6f2e3e2f11e7f8386d67de4290
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-12-23 13:38:41 +0100

    Packages updated

--- src/components/Packages/index.tsx
+++ src/components/Packages/index.tsx
@@ -5,19 +5,25 @@ import iceland from "../../assets/iceland.jpeg";
 const Packages = () => (
   <section id="packages" className="mt-5">
     <h2 className="text-center my-5 fs-1">Packages</h2>
-    <div className="row m-5">
+    <div className="row m-5 row-gap-5">
       <div className="col-lg">
         <div className="card">
           <img src={tromso} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">Tromso, Norway - $899</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+              laborum earum pariatur. Debitis nihil pariatur culpa voluptatibus
+              et quas amet architecto libero sequi molestias. Necessitatibus
+              nulla maiores dolor? Voluptate, ipsa!
             </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+            <button
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#booking-modal"
+            >
+              Book tour
+            </button>
           </div>
         </div>
       </div>
@@ -25,14 +31,20 @@ const Packages = () => (
         <div className="card">
           <img src={iceland} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">Reykjavik, Iceland - $799</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+              aperiam iure corporis amet architecto voluptatem saepe illum minus
+              consequuntur! Commodi ut ad rerum cumque omnis. Laboriosam
+              aspernatur est nihil maxime!
             </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+            <button
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#booking-modal"
+            >
+              Book tour
+            </button>
           </div>
         </div>
       </div>
@@ -40,14 +52,20 @@ const Packages = () => (
         <div className="card">
           <img src={yukon} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">Yukon, Canada - $849</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+              possimus id maxime natus excepturi omnis animi. Quod asperiores,
+              blanditiis quam, adipisci dolore rem assumenda doloremque mollitia
+              omnis possimus ipsum sequi.
             </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+            <button
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#booking-modal"
+            >
+              Book tour
+            </button>
           </div>
         </div>
       </div>
