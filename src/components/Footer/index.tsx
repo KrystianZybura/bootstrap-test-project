5df607741780112725611fd77022fdeaffb9c8a7
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-12-24 13:13:09 +0100

    added copyrights message for footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,7 @@
-const Footer = () => <footer className="footer bg-dark p-5"></footer>;
+const Footer = () => (
+  <footer className="footer bg-dark p-5 text-white">
+    <p className="text-center my-auto">All rights reserved</p>
+  </footer>
+);
 
 export default Footer;
